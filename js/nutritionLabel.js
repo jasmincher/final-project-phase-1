e5946a82a43be8952ba8890c786327b5ae88aca9
Jas <c.jasminher@gmail.com>
2021-12-02 15:24:53 -0500

    Changed endpoint so that it can get nutrition label image instead of nutrition label html document.

--- js/nutritionLabel.js
+++ js/nutritionLabel.js
@@ -1,36 +1,32 @@
 import axios from "axios";
 import dotenv from "dotenv";
-import {testVariable} from '../js/test'
+import { testVariable } from "../js/test";
 dotenv.config();
 
 const api_key = process.env.API_KEY;
 const base_url = process.env.BASE_URL;
-let label;
 
 const showNutritionLabel = () => {
   axios
-    .get(`${base_url}/${testVariable}/nutritionLabel?apiKey=${api_key}`)
+    .get(`${base_url}/${testVariable}/nutritionLabel.png?apiKey=${api_key}`, { 
+      responseType: "blob", //we have to do this since we get characters returned instead of a url
+    })
     .then((response) => {
-        label = response.data;
-
-        nutritionLabel();
+      let imgURL = URL.createObjectURL(response.data); //here we create our url for our blob-file
+      nutritionLabel(imgURL);
     })
     .catch((error) => console.error(error));
 };
 
+const nutritionLabel = (src) => {
+  const cont = document.getElementById("nutrition-label-info");
 
+  const nutritionLabelImg = document.createElement("img");
+  nutritionLabelImg.setAttribute("src", src);
 
+  cont.appendChild(nutritionLabelImg);
+};
 
-
-const nutritionLabel = () => {
-   
-    const cont = document.getElementById("nutrition-label-info");
-
-    const div = document.createElement("div");
-    div.innerHTML = label;
-
-    cont.appendChild(div)
-  };
-  
-document.getElementById("nutrition-label-btn").addEventListener("click", showNutritionLabel)
-
+document
+  .getElementById("nutrition-label-btn")
+  .addEventListener("click", showNutritionLabel);
