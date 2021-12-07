a4792aed0634d48cfe191a734e451ec6b7af5930
Braun <julianpayne248@gmail.com>
2021-12-06 21:03:56 -0500

    Added function to create image, title, and description

--- trendingPage.js
+++ trendingPage.js
@@ -9,11 +9,8 @@ const getTrending = () => {
   axios
     .get(`${base_url}/random?number=10&apiKey=${api_key}`)
     .then((response) => {
-      const data = response.data;
-      console.log(data);
-      appendCards(response)
-      // export let data = createTitle(response.data.title)
-      // createDetailedView(response.data);
+      const data = response.data.recipes;
+      appendCards(data)
     })
     .catch((error) => console.error(error));
   };
@@ -23,30 +20,27 @@ const getTrending = () => {
 // IMAGE TAG, TITLE & DESCRIPTION
 const createCard = (title, desc, img) => {
   let container = document.getElementById("trending-recipes-container")
+  let div = document.createElement("div")
 
   let recipeName = document.createElement("h6")
-  recipeName.textContent = title;
+  recipeName.innerHTML = title;
 
   let description = document.createElement("p")
-  description.textContent = desc;
+  description.innerHTML = desc;
 
   let image = document.createElement("img")
   image.setAttribute("src", img)
 
-  container.appendChild(recipeName)
-  container.appendChild(description)
-  container.appendChild(image)
+  div.appendChild(recipeName)
+  div.appendChild(description)
+  div.appendChild(image)
+  return div
 }
 
 const appendCards = (recipes) => {
   let container = document.getElementById("trending-recipes-container")
     recipes.forEach((recipe) => {
       let card = createCard(recipe.title, recipe.summary, recipe.image)
-    });
-
+      container.appendChild(card)
+    })
 }
-//    const createTitle = (trendingRec) => {
-//     let div = document.getElementById("trending-recipes-container");
-// //     const h1 = document.createElement('h1');
-//
-// }
