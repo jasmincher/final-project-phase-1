e079a01b8aa5bfca1c5258a043e0129731bdd583
Jas <c.jasminher@gmail.com>
2021-12-09 20:19:33 -0500

    Added event listener to call getSearch function when search button is clicked

--- js/search.js
+++ js/search.js
@@ -5,50 +5,65 @@ dotenv.config();
 const api_key = process.env.API_KEY;
 const base_url = process.env.BASE_URL;
 
-const getSearch = () => {
+const getSearch = (id) => {
   axios
-    .get(`${base_url}/complexSearch?query=pizza&apiKey=${api_key}`)
+    .get(`${base_url}/complexSearch?query=${id}&number=100&apiKey=${api_key}`)
     .then((response) => {
-      const data = response.data.recipes;
-      const loading = document.createElement("h5")
-      loading.innerHTML = "Search any recipe here and find out how to make it! "
-      const container = document.getElementById("search-recipes-container")
+      const data = response.data.results;
+      let noResults = document.createElement("h5");
+      noResults.innerHTML = "No results found, try searching another recipe.";
+      const container = document.getElementById("recipes-list");
 
-      if (data) {
-       appendCards(data)
+      if (data.length > 0) {
+        //if there are results found then cards will render
+        appendCards(data);
       } else {
-       container.appendChild(loading)
+        //if no results are found noResults message will appear
+        container.appendChild(noResults);
       }
     })
     .catch((error) => console.error(error));
-  };
+};
 
-  getSearch();
+//event listener that will call getSearch function when
+//search button is clicked, and will clear elements inside
+//of parent element 'recipes list'
 
-  // IMAGE TAG, TITLE & DESCRIPTION
-  const createCard = (title, desc, img) => {
-    let container = document.getElementById("search-recipes-container")
-    let div = document.createElement("div")
-
-    let recipeName = document.createElement("h6")
-    recipeName.innerHTML = title;
-
-    let description = document.createElement("p")
-    description.innerHTML = desc;
-
-    let image = document.createElement("img")
-    image.setAttribute("src", img)
-
-    div.appendChild(recipeName)
-    div.appendChild(description)
-    div.appendChild(image)
-    return div
+document.getElementById("search-btn").addEventListener("click", () => {
+  let value = document.getElementById("search-input").value;
+  let myNode = document.getElementById("recipes-list");
+  while (myNode.firstChild) {
+    myNode.removeChild(myNode.lastChild);
   }
+  console.log(value);
+  getSearch(value);
+});
 
-  const appendCards = (recipes) => {
-    let container = document.getElementById("search-recipes-container")
-      recipes.forEach((recipe) => {
-        let card = createCard(recipe.title, recipe.summary, recipe.image)
-        container.appendChild(card)
-      })
-  }
+// IMAGE TAG & TITLE
+const createCard = (title, img, id) => {
+  let link = document.createElement("a");
+  let url = new URL("http://localhost:1234/pages/details.html");
+  url.searchParams.append("id", id);
+  link.setAttribute("href", url);
+
+  let div = document.createElement("div");
+
+  let recipeName = document.createElement("h6");
+  recipeName.innerHTML = title;
+
+  let image = document.createElement("img");
+  image.setAttribute("src", img);
+
+  link.appendChild(div);
+  div.appendChild(recipeName);
+  div.appendChild(image);
+  return link;
+};
+
+const appendCards = (recipes) => {
+  let container = document.getElementById("recipes-list");
+  recipes.forEach((recipe) => {
+    let card = createCard(recipe.title, recipe.image, recipe.id);
+    container.appendChild(card);
+  });
+};
